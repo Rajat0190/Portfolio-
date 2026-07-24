00f5dd55e7a9905dfe05d17111ff9802243eccd7
Rajat0190 <rajatgautam0190@gmail.com>
2026-07-25 03:56:41 +0530

    Fix TypeScript compilation build errors for Vercel deployment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+declare module "gsap-trial/SplitText" {
+  const SplitText: any;
+  export { SplitText };
+  export default SplitText;
+}
+
+declare module "gsap-trial/ScrollSmoother" {
+  const ScrollSmoother: any;
+  export { ScrollSmoother };
+  export default ScrollSmoother;
+}
+
+declare module "gsap-trial/*" {
+  const content: any;
+  export default content;
+}
