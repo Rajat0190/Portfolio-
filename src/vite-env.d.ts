5a47effeaf8ee190be150c1fbcb5a54245764e37
Rajat0190 <rajatgautam0190@gmail.com>
2026-07-25 21:35:23 +0530

    fix: replace deprecated gsap-trial package with standard gsap

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,18 +1,18 @@
 /// <reference types="vite/client" />
 
-declare module "gsap-trial/SplitText" {
+declare module "gsap/SplitText" {
   const SplitText: any;
   export { SplitText };
   export default SplitText;
 }
 
-declare module "gsap-trial/ScrollSmoother" {
+declare module "gsap/ScrollSmoother" {
   const ScrollSmoother: any;
   export { ScrollSmoother };
   export default ScrollSmoother;
 }
 
-declare module "gsap-trial/*" {
+declare module "gsap/*" {
   const content: any;
   export default content;
 }
